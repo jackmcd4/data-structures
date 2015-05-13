37c938e737af38c85a48e6e8acacaa6caa80a1a9
jackmcd4 <jackmcdevitt4@gmail.com>
2015-05-13 16:38:41 -0700

    Up to queue-shared behavior

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,29 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  var initiate = {
+    sizes : 0,
+    storage : {}
+  }
+  _.extend(initiate, stackMethods);
+ return initiate;
 };
 
-var stackMethods = {};
-
+var stackMethods ={
+  pop : function(){
+    this.sizes--;
+    var result = this.storage[this.sizes];
+    delete this.storage[this.sizes];
+    return result;
+  },
+  push : function(value){
+    this.storage[this.sizes]=value;
+    this.sizes++;},
+  size : function(){
+  if(this.sizes<0){
+    return 0;
+  }
+    return this.sizes;}
+};
 
--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,34 +3,36 @@ var Queue = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
-
-  var size =0;
+  var size = 0;
+  var deleted;
   // Implement the methods below
 
   someInstance.enqueue = function(value){
-
     storage[size]=value;
     size++;
   };
 
   someInstance.dequeue = function(){
-
-
-    var result = storage[0]
-    if(size>0){
-      for (var i=0; i<size; i++){
+     var result= storage[0];
+     if(size>0){
+      for(var i=0; i<size; i++){
         storage[i]=storage[i+1];
       }
       delete storage[size];
-      size--;
-    }
-    return result;
-
-  };
+        size--;
+     }
+     return result;
+    };
 
   someInstance.size = function(){
+    if(size<0){
+      return 0;
+    }
     return size;
   };
 
   return someInstance;
 };
+
+//arr1=[0, 1, 2]
+//arr2=['a', 'b', 'c']
