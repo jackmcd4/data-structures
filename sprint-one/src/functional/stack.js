2dc4ecceca2188af438c66c3380e96d92cd13d92
jackmcd4 <jackmcdevitt4@gmail.com>
2015-05-13 14:34:39 -0700

    Reworked stack

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,25 +3,26 @@ var Stack = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
+
   var size=0;
 
   // Implement the methods below
   someInstance.push = function(value){
     storage[size]=value;
     size++;
+
   };
 
   someInstance.pop = function(){
-    var result=storage[size-1];
-    if(size>0){
-      delete storage[size-1];
-      size--;
-    }
+    size--;
+    return storage[size];
 
-    return result;
   };
 
   someInstance.size = function(){
+    if(size<0){
+      return 0;
+    }
     return size;
   };
 
