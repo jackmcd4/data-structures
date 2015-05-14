b131a92a58370d44f2614463eeee7172f72c1cff
jackmcd4 <jackmcdevitt4@gmail.com>
2015-05-14 16:14:53 -0700

    finished link list!

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -2,14 +2,33 @@ var LinkedList = function(){
   var list = {};
   list.head = null;
   list.tail = null;
+  list.beforeHead=false;
 
   list.addToTail = function(value){
+    if(list.tail===null){
+      list.head=Node(value);
+      list.tail=list.head;
+    } else if(!list.beforeHead){
+      list.beforeHead = Node(value);
+      list.tail = list.beforeHead;
+    }else {
+      list.tail=Node(value);
+    }
   };
 
   list.removeHead = function(){
+    var result = list.head.value;
+    list.head = list.beforeHead;
+    return result;
   };
 
   list.contains = function(target){
+    for(var key in list){
+      if(list[key].value===target){
+        return true;
+      }
+    }
+    return false;
   };
 
   return list;
