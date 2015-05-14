c853a92b121e3898c9cf6af1ae50157f5302234c
jackmcd4 <jackmcdevitt4@gmail.com>
2015-05-13 20:26:16 -0700

    Up to prototypal pattern

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,9 +1,38 @@
 var Queue = function(){
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var instantiate = {
+    sizes: 0,
+    storage: {}
+  };
+  _.extend(instantiate, queueMethods);
+
+
+  return instantiate;
 };
 
-var queueMethods = {};
+var queueMethods = {
+
+  dequeue: function(){
+    var result = this.storage[0];
+    if(this.sizes>0){
+      for(var i=0; i<this.sizes; i++){
+        this.storage[i]=this.storage[i+1];
+      }
+    }
+    delete this.storage[this.sizes]
+    this.sizes--;
+    return result;
+    },
+  enqueue: function(value){
+    this.storage[this.sizes]=value;
+    this.sizes++;},
+  size: function(){
+    if(this.sizes<0){
+      return 0;
+    }
+    return this.sizes;}
+};
 
 
 
