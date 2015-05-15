c7b5d6f334c9716ca9a0647955f2d98ad805d023
jackmcd4 <jackmcdevitt4@gmail.com>
2015-05-14 21:19:44 -0700

    almost got treeeee!

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,10 +1,10 @@
 var Tree = function(value){
   var newTree = {};
   newTree.value = value;
-
+  newTree.count = 0;
   // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = [];  // fix me
+  _.extend(newTree, treeMethods);
   return newTree;
 };
 
@@ -15,10 +15,26 @@ var Tree = function(value){
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
-
+  this.children.push(Tree(value));
+  this.count++;
 };
 
 treeMethods.contains = function(target){
+  //does tree object.value === target?
+    //return true
+var recurse = function(node){//doesn't work because of scope...
+    for(var i = 0; i<node.length; i++){
+      if(node[i].value===target){
+        return true;
+      } else if(node[i].children.length!==0){
+        recurse(node[i].children);
+    }
+  }
+return false;
+}
+
+return recurse(this.children);
+
 
 };
 
