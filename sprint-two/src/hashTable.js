f2f833a61464a1eaf107788b96fe37a00722cb2a
jackmcd4 <jackmcdevitt4@gmail.com>
2015-05-16 10:32:26 -0700

    Completed Hash Table

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -4,28 +4,66 @@ var HashTable = function(){
 };
 
 HashTable.prototype.insert = function(k, v){
+  //if storage at i is undefined
+    // set that
+  //else
+    //for loop
+      // if storage at i[j][0] === k
+        // replace
+  //else
+
+    //get current array at storage[i], create new array and for loop through storage[i] pushing 2ples
+      //add new value to new array
+      // storage[i].set new array with ALL 2ples
 
   var i = getIndexBelowMaxForKey(k, this._limit);
-  //if in storage i k already exists, we replace v. if k in i does not exist but there's another array in i, add [k, v]
-    //put i into storage as the key that references both k and v [k, v]
-//if k at i exists and doesn't cancel call .get, change to array with 2 arrays, call .set
+  var truthy = false;
+  if(this._storage.get(i) === undefined){
     this._storage.set(i, [k, v]);
+    truthy = true;
+  } else {
+    for (var j=0; j<this._storage.get(i).length; j++){
+      if(this._storage.get(i)[j][0] === k){
+        this._storage.set(i[j],[k,v]);
+        truthy = true;
+      }
+    }
+  }
+  if(truthy === false){
+    var myArr = [];
+    for(var j=0; j<this._storage.get(i).length; j++){
+      myArr.push(this._storage.get(i));
+    }
+    myArr.push([k,v]);
+    this._storage.set(i,myArr);
+  }
+
 
 
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  // for(var j=0; j<this._storage.length; j++){
-  //   if(this._storage[j] === i){
-  //     return this._storage[j][1];
-  //   }
-  // }
-  return this._storage.get(i)[1];
-  return null;
+
+  if(this._storage.get(i)===null){
+    return null;
+  }
+  if(typeof this._storage.get(i)[0] === 'string'){
+    return this._storage.get(i)[1];
+  } else{
+    for (var j=0; j<this._storage.get(i).length; j++){
+      if(this._storage.get(i)[j][0] === k){
+        return this._storage.get(i)[j][1];
+      }
+    }
+  }
 };
 
 HashTable.prototype.remove = function(k){
+  var i = getIndexBelowMaxForKey(k, this._limit);
+  if(this._storage.get(i)[0] === k){
+    this._storage.set(i, null);
+  }
 
 };
 
